62cb6b961def5949ba2dcf96d9a513e18eb5d565
Daniel Freiling <dfg@nota.dk>
2017-09-06 10:01:31 +0200

    chore: demo for new app-settings implementation

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -2,9 +2,19 @@ import * as observable from 'tns-core-modules/data/observable';
 import * as pages from 'tns-core-modules/ui/page';
 import {HelloWorldModel} from './main-view-model';
 
+var context: HelloWorldModel;
+
 // Event handler for Page 'loaded' event attached in main-page.xml
 export function pageLoaded(args: observable.EventData) {
     // Get the event sender
     let page = <pages.Page>args.object;
-    page.bindingContext = new HelloWorldModel();
+    context = page.bindingContext = new HelloWorldModel();
+}
+
+export function runDefaultTest(args: observable.EventData) {
+    context.runDefaultTest();
+}
+
+export function runAsyncTest(args: observable.EventData) {
+    context.runAsyncTest();
 }
