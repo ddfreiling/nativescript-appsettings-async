f95e05423065eeb52d3bd431ee543a02a8b379b0
Daniel Freiling <dfg@nota.dk>
2017-09-06 11:25:25 +0200

    fix: typing was missing getter default value

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,8 @@
 export declare class AppSettingsAsync {
   public static hasKey(key: string): boolean;
-  public static getString(key: string): string;
-  public static getBoolean(key: string): boolean;
-  public static getNumber(key: string): number;
+  public static getString(key: string, defaultValue?: string): string;
+  public static getBoolean(key: string, defaultValue?: boolean): boolean;
+  public static getNumber(key: string, defaultValue?: number): number;
   public static setString(key: string, value: string): void;
   public static setBoolean(key: string, value: boolean): void;
   public static setNumber(key: string, value: number): void;
